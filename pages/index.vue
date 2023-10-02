33f61fe4f61004a57e7f7fc191bc186273b06f78
samuelscn <samus355@gmail.com>
2023-10-02 10:50:59 -0300

    feat: initialize project

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>{{ homeConfig.title }}</h1>
+  </header>
+  <body>
+    <p>{{ homeConfig.body }}</p>
+  </body>
+</template>
+
+<script setup lang="ts">
+import config1 from '~/infra/config-inst-1.json';
+import config2 from '~/infra/config-inst-2.json';
+
+type configuration = {
+  title?: string,
+  body?: string,
+}
+
+let homeConfig: configuration = reactive({});
+const hostname = ref('');
+
+onBeforeMount(() => {
+  // hostname.value = window.location.hostname
+  hostname.value = 'www.inst1.com';
+  if (hostname.value === 'localhost') {
+    homeConfig = config1;
+  } else {
+    homeConfig = config2;
+  }
+  // subdomain.value = hostname.value.split('.')[0];
+})
+</script>

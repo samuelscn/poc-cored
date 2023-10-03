f3569bf23a925fc1df91399959ee01ef5cb81563
samuelscn <samus355@gmail.com>
2023-10-03 10:09:53 -0300

    feat: site name

--- app.vue
+++ app.vue
@@ -5,8 +5,11 @@
 </template>
 
 <script setup lang="ts">
+const url = useRequestURL()
+
+const { data } = await useFetch(`/api/configurations/${url.hostname}`)
 useSeoMeta({
-  title: 'Site para testes institucionais',
+  title: data.value?.data.title,
   ogTitle: 'Site para testes institucionais',
   description: 'O site visa trabalhar com informações institucionais para crianças e adultos que desejam praticar atividades fisicas',
   ogDescription: 'O site visa trabalhar com informações institucionais para crianças e adultos que desejam praticar atividades fisicas',
